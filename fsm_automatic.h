be05270d305de3df9c30aa7616820a6e7b7bdaa9
hoang6918 <80245864+hoang6918@users.noreply.github.com>
2022-11-30 20:25:31 +0700

    Add files via upload

---
+++ fsm_automatic.h
@@ -0,0 +1,20 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: Nov 29, 2022
+ *      Author: Acer
+ */
+
+#ifndef SRC_FSM_AUTOMATIC_H_
+#define SRC_FSM_AUTOMATIC_H_
+
+void fsm_automatic_run();
+void fsm_automatic_run2();
+
+#include "main.h"
+#include "timer.h"
+#include "global.h"
+#include "button.h"
+
+
+#endif /* SRC_FSM_AUTOMATIC_H_ */
---
+++ global.h
@@ -0,0 +1,29 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 29, 2022
+ *      Author: Acer
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+/*
+ * global.h
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: phamv
+ */
+#include"timer.h"
+#include"main.h"
+
+#define INIT		1
+#define AUTO_RED	2
+#define AUTO_GREEN	3
+#define AUTO_YELLOW	4
+#define AUTO_WHITE	5
+
+extern int status1;
+extern int status;
+
+#endif /* INC_GLOBAL_H_ */
